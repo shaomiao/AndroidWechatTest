239a4206f3456bb10f08f6539013fb5865e3b81e
shaomiao <shaomiaojava@163.com>
2016-09-20 10:47:34 +0800

    update

--- app/src/main/java/com/gomarket/android/goubianli/ConfigUtil.java
+++ app/src/main/java/com/gomarket/android/goubianli/ConfigUtil.java
@@ -4,7 +4,6 @@ package com.gomarket.android.goubianli;
  * Created by shaomiao on 2016-9-8.
  */
 public  class  ConfigUtil {
-    public static final String Wechat_Appid="wx9fd17784b344f703";
-    public static final String Wechat_AppSecret="4e79323de388edce2a076ed38884ef2e";
+
 
 }
