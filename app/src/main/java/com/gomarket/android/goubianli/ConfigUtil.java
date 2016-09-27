1493fa25165a2426041d51461168c10d42d0dee7
shaomiao <shaomiaojava@163.com>
2016-09-27 13:49:36 +0800

    update wechat pay

--- app/src/main/java/com/gomarket/android/goubianli/ConfigUtil.java
+++ app/src/main/java/com/gomarket/android/goubianli/ConfigUtil.java
@@ -5,6 +5,7 @@ package com.gomarket.android.goubianli;
  */
 public  class  ConfigUtil {
 //    public static final String Wechat_Appid="wxb4ba3c02aa476ea1";
-   
+    public static final String Wechat_Appid="";
+    public static final String Wechat_AppSecret="";
 
 }
